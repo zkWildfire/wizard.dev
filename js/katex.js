812dd29089ac70ef8a4f02ff1ca877353b8dd4dc
zkWildfire <zkWildfire@users.noreply.github.com>
2023-06-28 00:46:54 +0000

    Deployed c8eb93f with MkDocs version: 1.4.3

---
+++ js/katex.js
@@ -0,0 +1,11 @@
+// Code obtained from https://squidfunk.github.io/mkdocs-material/reference/math/#katex
+document$.subscribe(({ body }) => {
+	renderMathInElement(body, {
+		delimiters: [
+			{ left: "$$",  right: "$$",  display: true },
+			{ left: "$",   right: "$",   display: false },
+			{ left: "\\(", right: "\\)", display: false },
+			{ left: "\\[", right: "\\]", display: true }
+		],
+	})
+})
---
+++ js/mathjax.js
@@ -0,0 +1,17 @@
+// Code obtained from https://squidfunk.github.io/mkdocs-material/reference/math/#mathjax
+window.MathJax = {
+	tex: {
+		inlineMath: [["\\(", "\\)"]],
+		displayMath: [["\\[", "\\]"]],
+		processEscapes: true,
+		processEnvironments: true
+	},
+	options: {
+		ignoreHtmlClass: ".*|",
+		processHtmlClass: "arithmatex"
+	}
+};
+
+document$.subscribe(() => {
+	MathJax.typesetPromise()
+})
