ca7818946011cd157a306dea1871c1259ff8d0a6
Zach Wilson <zach.wilson@wizard.dev>
2023-08-01 23:57:57 +0000

    Finish WriteThroughCacheLine tests

--- projects/matrix-cache-sim/test/SimulatorTests/CacheLines/WriteThroughCacheLineTests.cs
+++ projects/matrix-cache-sim/test/SimulatorTests/CacheLines/WriteThroughCacheLineTests.cs
@@ -3,6 +3,7 @@
  *   All rights reserved.
  */
 using Mcs.Simulator.CacheLines;
+using Mcs.Simulator.Events;
 using Mcs.Simulator.Memory;
 namespace McsTests.Simulator.CacheLines;
 
@@ -53,5 +54,113 @@ public class WriteThroughCacheLineTests
 		var memory = Mock.Of<IMemory>();
 		var cacheLine = new WriteThroughCacheLine(memory, startingAddress, size);
 		Assert.False(cacheLine.Contains(address));
+	}
+
+	[Fact]
+	public void ReadValidMemoryAddress()
+	{
+		const int ADDRESS = 1;
+		const int VALUE = 2;
+		var mockMemory = new Mock<IMemory>();
+		mockMemory.Setup(x => x.Read(ADDRESS))
+			.Returns(VALUE);
+
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+		Assert.Equal(VALUE, cacheLine.Read(ADDRESS));
+	}
+
+	[Fact]
+	public void ReadValidMemoryAddressEmitsAccessedEvent()
+	{
+		const int ADDRESS = 1;
+		const int VALUE = 2;
+		var mockMemory = new Mock<IMemory>();
+		mockMemory.Setup(x => x.Read(ADDRESS))
+			.Returns(VALUE);
+
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+		OnCacheLineAccessedEventArgs? args = null;
+		cacheLine.OnCacheLineAccessed += (_, a) => args = a;
+
+		cacheLine.Read(ADDRESS);
+		Assert.NotNull(args);
+		Assert.Equal(ADDRESS, args.Address);
+		Assert.True(args.IsRead);
+		Assert.Equal(VALUE, args.OldValue);
+		Assert.Equal(VALUE, args.NewValue);
+	}
+
+	[Fact]
+	public void ReadInvalidMemoryAddressThrows()
+	{
+		var mockMemory = new Mock<IMemory>();
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+
+		Assert.Throws<ArgumentOutOfRangeException>(
+			() => cacheLine.Read(-1));
+		Assert.Throws<ArgumentOutOfRangeException>(
+			() => cacheLine.Read(123));
+		mockMemory.Verify(x => x.Read(It.IsAny<int>()), Times.Never);
+	}
+
+	[Fact]
+	public void WriteValidMemoryAddress()
+	{
+		const int ADDRESS = 1;
+		const int VALUE = 2;
+		var mockMemory = new Mock<IMemory>();
+		mockMemory.Setup(x => x.Write(ADDRESS, VALUE));
+
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+		cacheLine.Write(ADDRESS, VALUE);
+		mockMemory.Verify(x => x.Write(ADDRESS, VALUE), Times.Once);
+	}
+
+	[Fact]
+	public void WriteValidMemoryAddressEmitsAccessedEvent()
+	{
+		const int ADDRESS = 1;
+		const int VALUE = 2;
+		var mockMemory = new Mock<IMemory>();
+		mockMemory.Setup(x => x.Read(ADDRESS))
+			.Returns(VALUE);
+
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+		OnCacheLineAccessedEventArgs? args = null;
+		cacheLine.OnCacheLineAccessed += (_, a) => args = a;
+
+		cacheLine.Write(ADDRESS, VALUE);
+		Assert.NotNull(args);
+		Assert.Equal(ADDRESS, args.Address);
+		Assert.False(args.IsRead);
+		Assert.Equal(VALUE, args.OldValue);
+		Assert.Equal(VALUE, args.NewValue);
+	}
+
+	[Fact]
+	public void WriteInvalidMemoryAddressThrows()
+	{
+		var mockMemory = new Mock<IMemory>();
+		var cacheLine = new WriteThroughCacheLine(mockMemory.Object, 0, 4);
+
+		Assert.Throws<ArgumentOutOfRangeException>(
+			() => cacheLine.Write(-1, 0));
+		Assert.Throws<ArgumentOutOfRangeException>(
+			() => cacheLine.Write(123, 0));
+		mockMemory.Verify(x => x.Write(
+			It.IsAny<int>(),
+			It.IsAny<int>()),
+			Times.Never
+		);
+	}
+
+	[Fact]
+	public void CodeCoverageOnly()
+	{
+		var memory = Mock.Of<IMemory>();
+		var cacheLine = new WriteThroughCacheLine(memory, 0, 4);
+
+		// This method does nothing
+		cacheLine.Flush();
 	}
 }
