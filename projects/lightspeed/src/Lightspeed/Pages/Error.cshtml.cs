009c223b05375ec349d8b3fa8695f2d369484e8c
Zach Wilson <zach.wilson@wizard.dev>
2023-08-26 00:52:13 +0000

    Remove unnecessary autogenerated code, reformat autogenerated code

--- projects/lightspeed/src/Lightspeed/Pages/Error.cshtml.cs
+++ projects/lightspeed/src/Lightspeed/Pages/Error.cshtml.cs
@@ -3,21 +3,40 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Lightspeed.Pages;
 
+/// <summary>
+/// Page model for the error page.
+/// </summary>
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
 {
+	/// <summary>
+	/// Request ID that caused the error.
+	/// </summary>
 	public string? RequestId { get; set; }
 
+	/// <summary>
+	/// Whether or not the request ID should be shown.
+	/// </summary>
 	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
+	/// <summary>
+	/// Logger to write to.
+	/// </summary>
 	private readonly ILogger<ErrorModel> _logger;
 
+	/// <summary>
+	/// Initializes the error page model.
+	/// </summary>
+	/// <param name="logger">Logger to write to.</param>
 	public ErrorModel(ILogger<ErrorModel> logger)
 	{
 		_logger = logger;
 	}
 
+	/// <summary>
+	/// Called when the page is requested.
+	/// </summary>
 	public void OnGet()
 	{
 		RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
--- projects/lightspeed/src/Lightspeed/Program.cs
+++ projects/lightspeed/src/Lightspeed/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Builder;
-using Lightspeed.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
